240cf6b7e62803d603e097f89d91935e90c5d401
Juan Emilio Miralles <jsbto@Juans-MacBook-Pro.local>
2020-03-27 15:45:04 +0100

    Prove new method using ElementTree find

--- pubmed_abstracts_to_xlsx.py
+++ pubmed_abstracts_to_xlsx.py
@@ -27,6 +27,16 @@ def init_worksheet():
 
 ## -------------------------------------------------------------#
 
+#TODO :
+#
+# Replace this construction:
+#        if ArticleField.tag == 'Abstract':
+#              for elemAbstract in ArticleField:
+#                  if elemAbstract.tag == 'AbstractText':
+#                      Cell = 'E' + str(EntryIndex)
+#                      worksheet[Cell] = elemAbstract.text
+# with some relevant built-in search method.
+
 #
 # process article fields 
 #
@@ -45,6 +55,11 @@ def process_article(worksheet, Article, EntryIndex):
                 if elemAbstract.tag == 'AbstractText':
                     Cell = 'E' + str(EntryIndex)
                     worksheet[Cell] = elemAbstract.text
+        # if ArticleField.tag == 'Journal':
+        #     for elemJournal in ArticleField:
+        #         if elemAbstract.tag == 'AbstractText':
+        #             Cell = 'E' + str(EntryIndex)
+        #             worksheet[Cell] = elemAbstract.text
 
 #
 # process pmid fields 
@@ -72,18 +87,19 @@ def pm_xml2xlsx(infile, outfile, debug):
 
     EntryIndex = 1 # start populating after header
 
-    for PubmedArticle in root:
+    for PubmedArticle in root.iter('PubmedArticle'):
+        PubmedArticleField = PubmedArticle.find('MedlineCitation')
         #logging.debug("found PubmedArticle: {} (${})".format(self.name, self.price))
         logging.debug("found PubmedArticle")
         EntryIndex = EntryIndex + 1
-        for PubmedArticleField in PubmedArticle:
-            if PubmedArticleField.tag == 'MedlineCitation':
-                logging.debug('found MedlineCitation')
-                for SubField in PubmedArticleField:
-                    if SubField.tag == 'PMID':
-                        process_pmid(worksheet, SubField, EntryIndex)
-                    if SubField.tag == 'Article':
-                        process_article(worksheet, SubField, EntryIndex)
+        # for PubmedArticleField in PubmedArticle:
+        #     if PubmedArticleField.tag == 'MedlineCitation':
+        logging.debug('found MedlineCitation')
+        for SubField in PubmedArticleField:
+            if SubField.tag == 'PMID':
+                process_pmid(worksheet, SubField, EntryIndex)
+            if SubField.tag == 'Article':
+                process_article(worksheet, SubField, EntryIndex)
 
 
     # Save the file
