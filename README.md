078d61e76f9c26d892131f0a6ed3548f2892eab8
Juan Emilio Miralles <jsbto@Juans-MacBook-Pro.local>
2020-03-30 17:13:26 +0200

    Add README and setuptools script

---
+++ README.md
@@ -0,0 +1,5 @@
+pm_xml2xlsx
+
+PubMed XML to XLSX converter
+
+Simple tool to process a PubMed query XML output and store the relevant fields as XLSX.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="pm_xml2xlsx",
+    version="0.1",
+    packages=find_packages(),
+    # metadata to display on PyPI
+    author="Juan Emilio Miralles",
+    author_email="jemiralle@gmail.com",
+)
